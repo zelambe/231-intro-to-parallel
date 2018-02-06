0e8ec43d721d19717f5090744f42331b9acbcb8d
Dennis Cosgrove <cosgroved@wustl.edu>
2018-02-06 10:39:19 -0600

    replaced __STUDENT_NAME__ with Zahra Lambe

--- src/main/java/fibonacci/fun/RoundPhiToTheNOverSqrt5SequentialFibonacciCalculator.java
+++ src/main/java/fibonacci/fun/RoundPhiToTheNOverSqrt5SequentialFibonacciCalculator.java
@@ -31,7 +31,7 @@ import edu.wustl.cse231s.goldenratio.GoldenRatioUtils;
 import fibonacci.core.FibonacciCalculator;
 
 /**
- * @author __STUDENT_NAME__
+ * @author Zahra Lambe
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 public class RoundPhiToTheNOverSqrt5SequentialFibonacciCalculator implements FibonacciCalculator {
