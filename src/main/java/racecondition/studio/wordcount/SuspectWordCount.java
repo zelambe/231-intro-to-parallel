9d7eb3431b4f06329af63d59b409b54a463fba50
Dennis Cosgrove <cosgroved@wustl.edu>
2018-02-13 04:47:48 -0600

    replaced __STUDENT_NAME__ with Zahra Lambe

--- src/main/java/racecondition/studio/wordcount/SuspectWordCount.java
+++ src/main/java/racecondition/studio/wordcount/SuspectWordCount.java
@@ -31,7 +31,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 
 /**
- * @author __STUDENT_NAME__
+ * @author Zahra Lambe
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 public class SuspectWordCount {
