ba4482c82e23720bc32c57abd9c72f32c869e87c
Dennis Cosgrove <cosgroved@wustl.edu>
2018-02-15 11:33:01 -0600

    replaced __STUDENT_NAME__ with Zahra Lambe

--- src/main/java/matrixmultiply/fun/SubMatrix.java
+++ src/main/java/matrixmultiply/fun/SubMatrix.java
@@ -30,7 +30,7 @@ import edu.wustl.cse231s.NotYetImplementedException;
 import matrixmultiply.core.MatrixUtils;
 
 /**
- * @author __STUDENT_NAME__
+ * @author Zahra Lambe
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 public final class SubMatrix {
