328e45f69b70240e814b40e3127798817df60c19
Dennis Cosgrove <cosgroved@wustl.edu>
2018-03-27 12:12:25 -0500

    replaced __STUDENT_NAME__ with Zahra Lambe

--- src/main/java/sudoku/lab/ParallelSudoku.java
+++ src/main/java/sudoku/lab/ParallelSudoku.java
@@ -45,7 +45,7 @@ import sudoku.core.SquareSearchAlgorithm;
  * {@link #solveKernel(MutableObject, ImmutableSudokuPuzzle, SquareSearchAlgorithm)}
  * method, starting off the recursion.
  * 
- * @author __STUDENT_NAME__
+ * @author Zahra Lambe
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 public class ParallelSudoku {
